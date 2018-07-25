5e1c94316ec4ce6829ecf12643025efd23607098
王鹏程 <1007991483@qq.com>
2018-07-25 17:00:38 +0800

    add some support

---
+++ app/src/main/java/edu/tjrac/swant/todo/view/ToDoViewerActivity.java
@@ -0,0 +1,13 @@
+package edu.tjrac.swant.todo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDoViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_viewer);
+    }
+}
